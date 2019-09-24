bb3e438c2e10e560fed415d088eeebfcbca0d93d
grindpants <mustaqinishak2@gmail.com>
2019-09-24 09:22:52 +0700

    perbaikan exercise 6

--- exercise-6.js
+++ exercise-6.js
@@ -43,15 +43,21 @@ for(let i = 1; i <= 100; i++){
 for(let i = 1; i <= 100; i+=2){
   if(i % 3 == 0){
     console.log(i + " KELIPATAN 3") 
+  } else {
+    console.log("");
   }
 }
 for(let i = 1; i <= 100; i+=5){
   if(i % 6 == 0){
     console.log(i + " KELIPATAN 6") 
+  } else {
+    console.log("");
   }
 }
 for(let i = 1; i <= 100; i+=9){
-    if(i % 10 == 0){
-      console.log(i + " KELIPATAN 10") 
-    }
+  if(i % 10 == 0){
+    console.log(i + " KELIPATAN 10") 
+  } else {
+    console.log("");
   }
+}
