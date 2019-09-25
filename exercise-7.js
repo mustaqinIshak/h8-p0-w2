0ed41e2556114349917888a0d812b27038374237
grindpants <mustaqinishak2@gmail.com>
2019-09-25 15:26:49 +0700

    exercises 7

--- exercise-7.js
+++ exercise-7.js
@@ -1,4 +1,25 @@
 var rows1 = 5;
-for(let i = 1; i <= 5; i++){
-    console.log("*");
+// var tampungan = ''
+for(let i = 1; i <= rows1; i++){
+      console.log("*");
+}
+
+var rows2 = 5;
+// var tampungan = ''
+for(let i = 1; i <= rows2; i++){
+  var tampungan = ''
+      for(let j = 1; j <=5; j++){
+        tampungan += '*'
+      }
+    console.log(tampungan)
+}
+
+var rows3 = 5;
+// var tampungan = ''
+for(let i = 1; i <= rows3; i++){
+  var tampungan = ''
+      for(let j = 1; j <=i; j++){
+        tampungan += '*'
+      }
+    console.log(tampungan)
 }
